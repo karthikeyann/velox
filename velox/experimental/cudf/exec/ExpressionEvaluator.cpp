0829e156bc5386e7ee5b7e227042d29822d0f31b
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2025-06-16 05:37:08 -0500

    fix unsupported switch INTEGER

--- velox/experimental/cudf/exec/ExpressionEvaluator.cpp
+++ velox/experimental/cudf/exec/ExpressionEvaluator.cpp
@@ -481,6 +481,10 @@ cudf::ast::expression const& AstContext::pushExprToTree(
       auto const& op1d = tree.push(Operation{Op::CAST_TO_FLOAT64, op1});
       auto const& op2 = pushExprToTree(expr->inputs()[1]);
       return tree.push(Operation{Op::MUL, op1d, op2});
+    }  else if (c2 and c2->toString() == "0:INTEGER") {
+      auto const& op1 = pushExprToTree(expr->inputs()[0]);
+      auto const& op2 = pushExprToTree(expr->inputs()[1]);
+      return tree.push(Operation{Op::MUL, op1, op2});
     } else {
       VELOX_NYI("Unsupported switch complex operation " + expr->toString());
     }
